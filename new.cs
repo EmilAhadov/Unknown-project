2fb95a17ebdaab35e5f961484eb8c79363d7c09b
madPhysix <froggyrain123@gmail.com>
2023-02-23 19:41:22 +0400

    dasghasdihgiwsdhgsd

---
+++ new.cs
@@ -0,0 +1,5 @@
+int a =100;
+float b = 123.4f;
+double c = 213.3244d;
+decimal d = 2314.42323742m;
+
---
+++ new.js
@@ -0,0 +1,7 @@
+ let a = 100+4232
+let b = 1241241-14122
+let c = 213*23
+let d = 3**10
+let e = --c
+let f = ++di
+
